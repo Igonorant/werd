6d2e0f367e8931f3d89f2e6ca1aa01e981e55ee1
igolino3@hotmail.com <igorsmarques0@gmail.com>
2019-05-20 10:20:09 -0300

    Random generator of targer word

--- Werd/Werd/Main.cpp
+++ Werd/Werd/Main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <random>
 
 int main() {
 
@@ -12,9 +13,31 @@ int main() {
 		while (!in.eof()) {
 			std::string buffer;
 			std::getline(in, buffer);
-			words_database.push_back(buffer);
+			if (buffer.empty()) {
+				continue;
+			}
+			else {
+				words_database.push_back(buffer);
+			}
 		}
 	}
+
+	// random generate a target word based on a input seed
+	std::string target;
+	{
+		std::string seedString;
+		std::cout << "Please, enter a seed: ";
+		std::getline(std::cin, seedString);
+		std::seed_seq seed(seedString.begin(), seedString.end());
+		std::default_random_engine generator(seed);
+		std::uniform_int_distribution<int> distribution(0, words_database.size() - 1);
+		target = words_database[distribution(generator)];
+		std::cout << std::endl;
+	}
+
+
+
+
 
 
 	return 0;

2442c0aa15897b29e3c22957dfbec7a02d1e8847
igolino3@hotmail.com <igorsmarques0@gmail.com>
2019-05-20 10:04:08 -0300

    Added database and read it into a vector

--- Werd/Werd/Main.cpp
+++ Werd/Werd/Main.cpp
@@ -1,7 +1,20 @@
-
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
 
 int main() {
 
+	// read words database and store in a vector
+	std::vector<std::string> words_database;
+	{
+		std::fstream in("sgb-words.txt");
+		while (!in.eof()) {
+			std::string buffer;
+			std::getline(in, buffer);
+			words_database.push_back(buffer);
+		}
+	}
 
 
 	return 0;
